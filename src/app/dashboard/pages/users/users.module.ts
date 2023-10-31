ba4e3f0d72222e74efb1b06085f30f8b376ad0d7
Abrahamv19 <hugoabrahamv19@gmail.com>
2023-10-31 11:52:24 -0500

    add api rest

---
+++ src/app/dashboard/pages/users/users.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersComponent } from './users.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { UsersDialogComponent } from './components/users-dialog/users-dialog.component';
+import { UsersTableComponent } from './components/users-table/users-table.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    UsersComponent,
+    UsersDialogComponent,
+    UsersTableComponent,
+    UserDetailComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ],
+  exports: [
+    UsersComponent
+  ]
+})
+export class UsersModule { }
